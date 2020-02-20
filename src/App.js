8cbfcd7ccd4b61f225218a141cb0cfec55f6ee65
ramirihani <ramy.rayhani@gmail.com>
2020-02-20 14:47:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Search from "./component/Search";
+import MovieList from "./component/MovieList";
+import Rating from "./component/Rating";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const movies = [
+  {
+    id: "joker",
+    title: "joker",
+    rating: "4",
+    image: "https://www.dw.com/image/50340387_303.jpg"
+  },
+  {
+    id: "sonic",
+    title: "sonic",
+    rating: "5",
+    image: "https://i.ytimg.com/vi/lDfHbfVS8kI/maxresdefault.jpg"
+  },
+  {
+    id: "venom",
+    title: "venom",
+    rating: "3",
+    image:
+      "https://wegotthiscovered.com/wp-content/themes/wgtc_v2/resizer/resizer.php?file=uploads/2018/08/Venom-JP-poster-2-640x321.jpg&height=650&width=1219.1489361702&action=resize"
+  }
+];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      minRating: 1,
+      moviesArray: movies,
+      titleSearch: ""
+    };
+  }
+  addNewMovie(newMovie) {
+    this.setState({
+      ...this.state,
+      moviesArray: [...this.state.moviesArray, newMovie]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Search
+          value={this.state.titleSearch}
+          onChange={newTitle => {
+            this.setState({
+              titleSearch: newTitle
+            });
+          }}
+        />
+        <Rating
+          className="rating"
+          etoile={this.state.minRating}
+          onChange={newRating => {
+            this.setState({
+              minRating: newRating
+            });
+          }}
+        />
+        <MovieList
+          className="Card"
+          movies={this.state.moviesArray.filter(
+            el =>
+              el.rating >= this.state.minRating &&
+              el.title
+                .trim()
+                .toLowerCase()
+                .includes(this.state.titleSearch.trim().toLowerCase())
+          )}
+          onAddMovie={newMovie => this.addNewMovie(newMovie)}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/MovieList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import MovieCard from "./MovieCard";
+
+const MovieList = ({ movies, onAddMovie = () => {} }) => (
+  <div className="movie-list">
+    {movies.map((movie, key) => (
+      <MovieCard className="Cards" movie={movie} key={key} />
+    ))}
+    <div
+      className="addbtn"
+      onClick={() => {
+        onAddMovie({
+          id: Date.now(),
+          title: prompt("movie title: "),
+          rating: Number(prompt("movie rating: ")),
+          image: prompt("movie image: ")
+        });
+      }}
+    >
+      +
+    </div>
+  </div>
+);
+
+export default MovieList;
